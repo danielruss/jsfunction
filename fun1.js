5457b1e80bf8044ac4e720f1086f1464a55d57fb
danielruss <druss@mail.nih.gov>
2022-04-22 09:08:27 -0400

    added prod

--- fun1.js
+++ fun1.js
@@ -1,5 +1,12 @@
-export function sum(...args) {
+function sum(...args) {
     return args.reduce((previousValue, currentValue) => {
         return previousValue + currentValue
     })
 }
+function prod(...args) {
+    return args.reduce((previousValue, currentValue) => {
+        return previousValue * currentValue
+    })
+}
+
+export { sum, prod }
